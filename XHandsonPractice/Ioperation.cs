aceb86c3db454bb99b408b6c8fd7fe2e729d7d18
sanjay7900 <101556258+sanjay7900@users.noreply.github.com>
2022-06-02 15:58:40 +0530

    Update Ioperation.cs

--- XHandsonPractice/Ioperation.cs
+++ XHandsonPractice/Ioperation.cs
@@ -10,7 +10,7 @@ namespace XHandsonPractice
     {
         double add(double a,double b);
         double sub(double a,double b);
-        double multi(double a,double b);
+        double mul(double a,double b);
         double div(double a,double b);
 
     }
