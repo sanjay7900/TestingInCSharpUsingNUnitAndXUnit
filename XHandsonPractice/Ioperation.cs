2bc77d43cb50c517f3b197e8b7f76f012c2b3185
sanjay7900 <singhsanjay790043@gmail.com>
2022-06-02 15:21:40 +0530

    initial commit

---
+++ XHandsonPractice/Ioperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHandsonPractice
+{
+    public interface Ioperation
+    {
+        double add(double a,double b);
+        double sub(double a,double b);
+        double multi(double a,double b);
+        double div(double a,double b);
+
+    }
+}
---
+++ XHandsonPractice/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHandsonPractice
+{
+    public class Operations
+    {
+        public double Additions(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Substraction(double a, double b)
+        {
+            return a - b;
+
+        }
+
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
